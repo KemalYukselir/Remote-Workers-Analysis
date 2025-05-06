b97eea5d375c32d625ec6731f80b1d8bba266cdd
Kemal Yukselir <K.Yukselir123@gmail.com>
2025-05-06 21:24:04 +0100

    Added null check. Only show  with null values

--- eda.py
+++ eda.py
@@ -28,7 +28,21 @@ name_sort_df.columns = result
 for col in df.select_dtypes(include=['object']).columns:
     print(f"\n{col}: {df[col].unique()}\n")
 
+#####################
+### Check nulls
+#####################
 
+df_clean = name_sort_df.copy()
+missing = df_clean.isnull().sum()
+
+# Calculate % of missing values
+missing_percent = (missing / len(df_clean)) * 100
+
+# Filter and show only columns with missing data
+missing_percent = missing_percent[missing_percent > 0]
+
+# Format nicely
+print(missing_percent.sort_values(ascending=False).round(2))
 
 
 
