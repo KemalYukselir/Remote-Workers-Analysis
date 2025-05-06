66af2f2d26854f2b51d8b6f265f21e2628cc43df
Kemal Yukselir <K.Yukselir123@gmail.com>
2025-05-06 22:45:12 +0100

    No need to adjust col names - removed

--- eda.py
+++ eda.py
@@ -4,25 +4,10 @@ import pandas as pd
 ### Import CSV 
 ##################
 
-df = pd.read_csv('data/2020_rws.csv', encoding='ISO-8859-1')
+df = pd.read_csv('data/data.csv', encoding='ISO-8859-1')
 
 #####################
-### Adjust col names
-#####################
-
-name_sort_df = df.copy()
-result = []
-for col in name_sort_df.columns:
-    if len(col) < 121:
-        result.append(col[0:120])
-    else:
-        result.append(col[-121:])
-
-name_sort_df.columns = result
-# name_sort_df.info()
-
-#####################
-### Check unique values
+### Check unique values 
 #####################
 
 for col in df.select_dtypes(include=['object']).columns:
@@ -32,7 +17,7 @@ for col in df.select_dtypes(include=['object']).columns:
 ### Check nulls
 #####################
 
-df_clean = name_sort_df.copy()
+df_clean = df.copy()
 missing = df_clean.isnull().sum()
 
 # Calculate % of missing values
