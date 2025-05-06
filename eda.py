e49bf05fb90b4c658917bedb322459ca9286d041
Kemal Yukselir <K.Yukselir123@gmail.com>
2025-05-06 21:21:46 +0100

    Check unique values in every column

--- eda.py
+++ eda.py
@@ -12,7 +12,6 @@ df = pd.read_csv('data/2020_rws.csv', encoding='ISO-8859-1')
 
 name_sort_df = df.copy()
 result = []
-# print(type(name_sort_df.columns))
 for col in name_sort_df.columns:
     if len(col) < 121:
         result.append(col[0:120])
@@ -20,7 +19,16 @@ for col in name_sort_df.columns:
         result.append(col[-121:])
 
 name_sort_df.columns = result
-name_sort_df.info()
+# name_sort_df.info()
+
+#####################
+### Check unique values
+#####################
+
+for col in df.select_dtypes(include=['object']).columns:
+    print(f"\n{col}: {df[col].unique()}\n")
+
+
 
 
 
