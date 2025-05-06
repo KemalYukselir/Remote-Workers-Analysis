c35aaab7fb89e800d8cb44b2349e6b1123a81266
Kemal Yukselir <K.Yukselir123@gmail.com>
2025-05-06 19:08:37 +0100

    Adjusted long column names

--- eda.py
+++ eda.py
@@ -1,10 +1,26 @@
 import pandas as pd
 
 ##################
-### Import CSV ###
+### Import CSV 
 ##################
 
-# pd.set_option('display.max_columns', None)
 df = pd.read_csv('data/2020_rws.csv', encoding='ISO-8859-1')
 
-print(df.info())
+#####################
+### Adjust col names
+#####################
+
+name_sort_df = df.copy()
+result = []
+# print(type(name_sort_df.columns))
+for col in name_sort_df.columns:
+    if len(col) < 121:
+        result.append(col[0:120])
+    else:
+        result.append(col[-121:])
+
+name_sort_df.columns = result
+name_sort_df.info()
+
+
+
