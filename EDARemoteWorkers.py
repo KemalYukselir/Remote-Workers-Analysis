5bc1eaaecf3c903d7534929404f80b89aa889b0b
Kemal Yukselir <K.Yukselir123@gmail.com>
2025-05-09 10:18:43 +0100

    Removed correlation matrix function. Added comments

--- EDARemoteWorkers.py
+++ EDARemoteWorkers.py
@@ -4,7 +4,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 class EDARemoteWorkers():
-    DEBUG = False
+    DEBUG = True
     def __init__(self):
         # Import CSV
         self.df = pd.read_csv('data/survey.csv')
@@ -12,8 +12,8 @@ class EDARemoteWorkers():
         self.drop_unused_columns()
         self.handle_nulls()
         self.check_unique()
-        # if self.DEBUG:
-        #     self.run()
+        if self.DEBUG:
+            self.summary_statistics()
     
     def check_unique(self):
         """ 
@@ -39,8 +39,10 @@ class EDARemoteWorkers():
             print(self.df_clean.isnull().sum())
 
     def drop_unused_columns(self):
+        """ Cols that provide no value """
         cols_to_drop = ["comments","Timestamp","state"]
         self.df_clean = self.df_clean.drop(columns=cols_to_drop)
+
     
     def summary_statistics(self):
         """ Check numerical stats of every column """
@@ -48,37 +50,6 @@ class EDARemoteWorkers():
         print(self.df_clean.describe())
         print(self.df_clean.info())
 
-    def correlation_matrix(self):
-        """
-        Display a zoomed-out correlation matrix to better fit the screen.
-        """
-
-        # Convert target to numeric if not already
-        # self.df_clean['Mental_Health_Condition'] = self.df_clean['Mental_Health_Condition'].astype('category').cat.codes
-
-        plt.figure(figsize=(14, 8))  # Wider and taller figure
-        correlation = self.df_clean.corr(numeric_only=True)
-
-        sns.heatmap(
-            correlation,
-            annot=True,
-            cmap='coolwarm',
-            fmt=".3f",
-            annot_kws={"size": 8},  # smaller font for annotations
-            cbar_kws={"shrink": 0.75}  # smaller colorbar
-        )
-
-        plt.title("Correlation Matrix with Target", fontsize=16)
-        plt.xticks(rotation=45, ha='right', fontsize=10)
-        plt.yticks(rotation=0, fontsize=10)
-        plt.tight_layout()  # Adjust layout to prevent clipping
-        plt.show()
-
-    def run(self):
-        """ Auto run when class is called """
-        self.summary_statistics()
-        self.correlation_matrix()
-    
     def get_dataframe(self):
         return self.df_clean
 
