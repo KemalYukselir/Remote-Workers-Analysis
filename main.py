ccac837718e1ce385d279634b55b510790fb8e57
Kemal Yukselir <K.Yukselir123@gmail.com>
2025-05-10 00:22:09 +0100

    Added model pre load

--- main.py
+++ main.py
@@ -4,3 +4,9 @@ st.set_page_config(layout="wide")
 import numpy as np
 from ModelRemoteWorkerAnalysis import ModelRemoteWorkerAnalysis
 
+@st.cache_resource
+def load_model():
+    return ModelRemoteWorkerAnalysis()
+
+# Preload cached resources
+model = load_model()
