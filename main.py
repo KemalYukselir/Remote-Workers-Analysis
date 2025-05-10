4c4c2b96d60f43cf9f2b9304ec56fc6a53f05d3b
Kemal Yukselir <K.Yukselir123@gmail.com>
2025-05-10 11:11:28 +0100

    Added project overview page

--- main.py
+++ main.py
@@ -24,3 +24,47 @@ st.sidebar.image("assets/RemoteWorkImg.jpeg", width=900)
 st.sidebar.markdown("**Checkout my other project:**")
 st.sidebar.markdown("[Student Certify Rate](https://student-certify-rate.streamlit.app/)")
 
+def project_overview_page():
+  st.title("📘 Project Overview 📘")
+
+  st.markdown("""
+  # 🌍 Remote Worker Analysis 🌍
+  ## By Kemal Yukselir
+
+  ### Description:
+  This project explores workplace survey data to understand what influences whether individuals seek support for burnout. 
+  It uses data analysis, decision tree modelling, and comment analysis to highlight how personal and workplace conditions relate to burnout support.
+              
+  ### References
+  - [OSMI](https://www.kaggle.com/datasets/osmi/mental-health-in-tech-survey)
+
+  ### Modules:
+  - Pandas  
+  - NumPy  
+  - Scikit-learn  
+  - Statsmodels  
+  - Category Encoders  
+  - Streamlit  
+  - Matplotlib  
+  - Seaborn
+  - itertools
+
+  ### Project Highlights:
+  With all ethical practise considered, this is the best model I can get with many reruns.
+
+  - 🧪 Training Accuracy: 0.8514
+  - 🧾 Testing Accuracy:  0.8649
+              
+  - 📊 Classification Report:
+                precision    recall  f1-score   support
+
+            0       0.79      0.97      0.87        35
+            1       0.97      0.77      0.86        39
+
+    accuracy                           0.86        74
+    macro avg       0.88      0.87      0.86        74
+    weighted avg    0.88      0.86      0.86        74
+  """)
+
+if page == "Project Overview":
+    project_overview_page()
