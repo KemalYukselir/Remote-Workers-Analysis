747eed38b83374a765938637e1c3d59ad6471268
Kemal Yukselir <K.Yukselir123@gmail.com>
2025-05-14 20:55:35 +0100

    removed unused imports

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import streamlit as st
 # Set page to perma wide
 st.set_page_config(layout="wide")
-import numpy as np
 import pandas as pd
 import pickle
 import streamlit.components.v1 as components
