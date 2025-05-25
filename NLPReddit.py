0a3ba368d7b3b07008b607552d78c5211d61a775
Kemal Yukselir <K.Yukselir123@gmail.com>
2025-05-25 21:22:47 +0100

    added functions to clean text.

--- NLPReddit.py
+++ NLPReddit.py
@@ -50,3 +50,31 @@ for post in results:
 
 # Transform into dataframe with columns 'title' and 'selftext'
 df = pd.DataFrame(clean_results)
+
+def regex_clean(txt, regex):
+    """Replace any text matching the regex
+
+    Parameters
+    ----------
+    txt : string
+        A text string that you want to parse and remove matches
+    regex : string
+        A text string of the regex pattern you want to match
+
+    Returns
+    -------
+    The same txt string with the matches removes
+    """
+
+    return " ".join(re.sub(regex, "", txt).split())
+
+def remove_emoji(string):
+    emoji_pattern = re.compile("["
+                           u"\U0001F600-\U0001F64F" # emoticons
+                           u"\U0001F300-\U0001F5FF" # symbols & pictographs
+                           u"\U0001F680-\U0001F6FF" # transport & map symbols
+                           u"\U0001F1E0-\U0001F1FF" # flags (iOS)
+                           u"\U00002702-\U000027B0"
+                           u"\U000024C2-\U0001F251"
+                           "]+", flags=re.UNICODE)
+    return emoji_pattern.sub(r'', string)
