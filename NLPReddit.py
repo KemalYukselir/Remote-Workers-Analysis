9ad822c0b1ef666b0ba69b6d5696678bc11090f2
Kemal Yukselir <K.Yukselir123@gmail.com>
2025-05-25 15:58:23 +0100

    added praw and basic request. Added env variables.

--- NLPReddit.py
+++ NLPReddit.py
@@ -1,5 +1,8 @@
 # Regex
 import re
+import os
+import praw
+from dotenv import load_dotenv
 
 # Conntect to api
 import requests # Connect to external websites
@@ -20,6 +23,21 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
 # Download text files to reuse
-nltk.download('stopwords') # Download list of stop words
-nltk.download('punkt_tab') # Download a list of punctuation
-nltk.download('punkt') # Download a list of punctuation
+# nltk.download('stopwords') # Download list of stop words
+# nltk.download('punkt_tab') # Download a list of punctuation
+# nltk.download('punkt') # Download a list of punctuation
+
+# Load env variables
+load_dotenv()
+
+# Replace these with your actual credentials
+reddit = praw.Reddit(
+    client_id=os.getenv('CLIENT_ID'),  
+    client_secret=os.getenv('CLIENT_SECRET'),
+    user_agent=os.getenv('USER_AGENT'))
+
+# Simple test: fetch 5 hot posts from r/Python
+for post in reddit.subreddit("Python").hot(limit=5):
+    print(f"Title: {post.title}")
+    print(f"Upvotes: {post.score}")
+    print("---")
